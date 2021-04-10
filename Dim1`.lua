38486b22bc513e85a5dabe73b9d23fe8223eef0a
CadenWhaley <60452014+CadenWhaley@users.noreply.github.com>
2021-04-09 21:15:07 -0400

    Update Dim1`.lua

--- Dim1`.lua
+++ Dim1`.lua
@@ -1,1 +1,9 @@
+function TPPlayerToEggAndGrab(EggNumber)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Scriptable.EggsModel[EggNumber].Interaction.CFrame
+	fireclickdetector(game:GetService("Workspace").Scriptable.EggsModel[EggNumber].Interaction.ClickDetector,0)
+end
 
+TPPlayerToEggAndGrab(1)
+TPPlayerToEggAndGrab(2)
+TPPlayerToEggAndGrab(3)
+TPPlayerToEggAndGrab(4)
